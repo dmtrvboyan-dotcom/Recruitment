c2cc41f246df4e62d87ea8af0ac67141e9d5f7f7
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2026-04-28 13:40:09 +0300

    testing

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -5,5 +5,5 @@
 export { Header } from "./header"
 export { Footer } from "./footer"
 export { ScrollReveal } from "./scroll-reveal"
-export { default as DynamicBackground } from "./dynamic-background"
+// export { default as DynamicBackground } from "./dynamic-background"
 export { SocialSidebar } from "./social-sidebar"
