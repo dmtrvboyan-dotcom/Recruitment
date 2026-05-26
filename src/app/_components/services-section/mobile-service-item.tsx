2a33c66b5dd7197fd0497870c95e07a41f72a4b6
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2026-05-26 10:39:13 +0300

    added border on mobile version

--- src/app/_components/services-section/mobile-service-item.tsx
+++ src/app/_components/services-section/mobile-service-item.tsx
@@ -23,10 +23,10 @@ export const MobileServiceItem = memo(function MobileServiceItem({
 
   return (
     <div
-      className={`relative overflow-hidden transition-colors duration-300
+      className={`relative overflow-hidden transition-colors duration-300"
         ${isFeature
-          ? "bg-brand-navy text-brand-white"
-          : `bg-brand-white text-brand-navy ${isOpen ? "bg-brand-coral/5" : ""}`
+          ? "bg-brand-navy text-brand-white  border-t-2 border-l-2 border-r-2"
+          : `bg-brand-white border-l-2 border-r-2 border-b-2 text-brand-navy ${isOpen ? "bg-brand-coral/5" : ""}`
         }
       `}
     >
