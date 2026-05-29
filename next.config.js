4d3bc45463d1e9ea26f59fe4c56e9ad2b493c8e2
Boyan Dimitrov <boyandimitrov1462@gmail.com>
2026-05-29 14:21:42 +0300

    chore: allow R2 image domain in next/image

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
     unoptimized: false,
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-2e44b458d83f45af99300367d0b52938.r2.dev",
+      },
+    ],
   },
   compiler: {
     removeConsole: true,
